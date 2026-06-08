0c4391de87fda09d4ee6b02ad203478e715b0e27
Kristian Skov <kris701kj@gmail.com>
2026-06-08 08:51:53 +0200

    Added a small serialization library, to help converting typical weird CW types

---
+++ CargoWiseReplicationAPIInterface.Serialization/OADatetimeConverter.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace Helvion.Helvware.Plugins.TrendhimIntegration.Converters.Serialization
+{
+	/// <summary>
+	/// A type converter to convert the OA datetime format to a <seealso cref="DateTime"/>
+	/// </summary>
+	public class OADatetimeConverter : DefaultTypeConverter
+	{
+		/// <summary>
+		/// Convert from OA format to a <seealso cref="DateTime"/>
+		/// </summary>
+		/// <param name="text"></param>
+		/// <param name="row"></param>
+		/// <param name="memberMapData"></param>
+		/// <returns></returns>
+		public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+		{
+			if (text == null || text == "")
+				return null;
+			var value = double.Parse(text);
+			return DateTime.FromOADate(value);
+		}
+	}
+}
